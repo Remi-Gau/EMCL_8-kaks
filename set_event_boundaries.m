d8bec5f4c7309e8b47d42f87430d96862954f714
Remi Gau <remi_gau@hotmail.com>
2019-07-03 13:46:32 +0300

    move scrolling line lower during boundari selection

--- set_event_boundaries.m
+++ set_event_boundaries.m
@@ -1,10 +1,12 @@
 function [frames] = set_event_boundaries(texids, win, win_w, win_h, opt)
 
-frames = [NaN];
+frames = NaN;
 
 drawn_circles = 0;
 
 oval_radius = win_w/75;
+
+line_height = win_h*19/20;
 
 count = numel(texids);
 
@@ -23,19 +25,19 @@ while 1
         currentindex = count-2;
     end
     
-    oval_rect = get_oval_rect(x, oval_radius, win_h);
+    oval_rect = get_oval_rect(x, oval_radius, line_height);
     
     % Draw texture 'currentindex'
     Screen('DrawTexture', win, texids(currentindex));
 
-    Screen('DrawLine', win , [0 0 0], 0, win_h*4/5, win_w, win_h*4/5, 4);
+    Screen('DrawLine', win , [0 0 0], 0, line_height, win_w, line_height, 4);
     
     Screen('FillOval', win, [0 0 0], oval_rect);
     
     % Add selected frames
     if ~isnan(frames)
         for i_oval = 1:numel(frames)
-            oval_rect = get_oval_rect(frames(i_oval), oval_radius*1.2, win_h);
+            oval_rect = get_oval_rect(frames(i_oval), oval_radius*1.2, line_height);
             Screen('FillOval', win, [0 0 255], oval_rect);
         end
     end
@@ -78,10 +80,10 @@ frames = round(frames/win_w*count);
 
 end
 
-function oval_rect = get_oval_rect(x, oval_radius, win_h)
+function oval_rect = get_oval_rect(x, oval_radius, line_height)
     oval_rect = [...
         x - oval_radius, ...
-        win_h*4/5 - oval_radius, ...
+        line_height - oval_radius, ...
         x + oval_radius, ...
-        win_h*4/5 + oval_radius];
+        line_height + oval_radius];
 end
