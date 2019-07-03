88f2496a43886c6751d16293e686cd695b111e40
Remi Gau <remi_gau@hotmail.com>
2019-07-03 14:39:37 +0300

    use getechostring to type response

--- type_answer.m
+++ type_answer.m
@@ -8,9 +8,6 @@ dst_rect = [...
     win_w/2 + win_w/4 ...
     win_h/2 + win_h/4];
 
-KbQueueCreate(response_box);
-KbQueueStart(response_box);
-
 DrawFormattedText(win, ...
     instruction_text, ...
     'center' , win_h/5, opt.text_color);
@@ -21,45 +18,8 @@ Screen('FrameRect', win, opt.text_color, ...
 
 Screen('Flip', win);
 
-while 1
-    
-    [pressed, first_press] = KbQueueCheck(response_box);
-
-    if pressed
-        
-        time_secs = first_press(find(first_press));
-        key_code = min(find(first_press));
-        key_name = KbName(key_code);
-        
-        if first_press(opt.return)
-            break
-        end
-        
-        if first_press(opt.esc)
-            clean_up(response_box)
-            return
-        end
-        
-        response_text = [response_text key_name];
-        
-        
-        DrawFormattedText(win, ...
-            instruction_text, ...
-            'center' , win_h/5, opt.text_color);
-        
-        Screen('FrameRect', win, opt.text_color, ...
-            dst_rect, ...
-            2);
-        
-        DrawFormattedText(win, ...
-            response_text, ...
-            'center' , 'center', opt.text_color);
-        
-        Screen('Flip', win);
-
-    end
-    
-end
-KbQueueRelease(response_box);
+[response_text, terminatorChar] = ...
+    GetEchoString(win, [instruction_text '\n\n'], ...
+    win_w/4, win_h/4, opt.text_color, opt.background, 0, response_box, dst_rect, win_h);
 
 end
